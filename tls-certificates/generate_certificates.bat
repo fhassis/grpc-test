9d0d4ba0055cce9408c8914fda9574149e29711c
fhassis <fhassis2@gmail.com>
2022-10-18 20:24:52 -0300

    removed certificate duration

--- tls-certificates/generate_certificates.bat
+++ tls-certificates/generate_certificates.bat
@@ -4,4 +4,4 @@ REM This script generates a self signed certificate with openssl for development
 
 set TLS_OUT=.
 
-openssl req -newkey rsa:2048 -nodes -keyout %TLS_OUT%/server.key -x509 -days 365 -out %TLS_OUT%/server.crt
+openssl req -newkey rsa:2048 -nodes -keyout %TLS_OUT%/server.key -x509 -out %TLS_OUT%/server.crt
