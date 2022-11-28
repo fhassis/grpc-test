6c8c0bd8cfcdbccea40b9e4300964fccb235d501
fhassis <fhassis2@gmail.com>
2022-11-27 21:27:48 -0300

    added grpcweb configuration.

--- grpc-dotnet/grpc-server/Program.cs
+++ grpc-dotnet/grpc-server/Program.cs
@@ -5,7 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+// Add CORS for GrpcWeb
+const string corsPolicy = "AllowAll";
+builder.Services.AddCors(o => o.AddPolicy(corsPolicy, builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+    }));
+
 var app = builder.Build();
+
+// Add GrpcWeb
+app.UseCors(corsPolicy);
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
